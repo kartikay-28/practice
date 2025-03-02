6d4864dfbc5ca81789cfe75bb2b31d6bf574cd29
kartikay-28 <kartikayrattan1@gmail.com>
2025-03-02 17:47:03 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -3,13 +3,23 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 
+const creator = [
+    {
+        Name: "Kartikay",
+        Age: 18,
+        hobbies: ["cricket", "coding", "gaming"],
+        skills: ["javascript", "cpp"]             
+    }
+];
+
 app.get('/', (req, res) => {
     res.send('<h1 style="text-align: center;">This is my Express server</h1>');
 });
 
 app.get('/Home', (req, res) => {
-    res.send('<h1 style="text-align: center;">Hi, my name is Kartikay Rattan</h1> <br> <p>Welcome! This is my practice for backend CA.</p>');
+    res.json({ "Message": "Hi, my name is Kartikay Rattan. Welcome to my practice for backend CA.", "Data": creator });
 });
+
 
 app.listen(PORT, () => {
     console.log(`Hi, my name is Kartikay Rattan. The server is live on http://localhost:${PORT}`);
