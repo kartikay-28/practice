b7d3afb68df40986eca4c656040ec2a4269516b1
kartikay-28 <kartikayrattan1@gmail.com>
2025-03-02 17:28:30 +0530

    made schema.js and corrected

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 
 const PORT = 3000;
-
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send('<h1 style="text-align: center;">This is my Express server</h1>')
-})
+app.get('/', (req, res) => {
+    res.send('<h1 style="text-align: center;">This is my Express server</h1>');
+});
 
-app.get('/Home',(req,res)=>{
-    res.send('<h1 style="text-align: center;">Hi, my name is Kartikay Rattan</h1> <br> <p> Welcome this is my practice for backend ca . ')
-})
+app.get('/Home', (req, res) => {
+    res.send('<h1 style="text-align: center;">Hi, my name is Kartikay Rattan</h1> <br> <p>Welcome! This is my practice for backend CA.</p>');
+});
 
-app.listen(PORT , () => {
-    console.log(`Hi my name is Kartikay Rattan . The server is live on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Hi, my name is Kartikay Rattan. The server is live on http://localhost:${PORT}`);
+});
