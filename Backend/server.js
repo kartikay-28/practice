308f5088c95483b6140192e64f8bd7fe64882ca5
kartikay-28 <kartikayrattan1@gmail.com>
2025-03-02 18:16:12 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -20,6 +20,20 @@ app.get('/Home', (req, res) => {
     res.json({ "Message": "Hi, my name is Kartikay Rattan. Welcome to my practice for backend CA.", "Data": creator });
 });
 
+//  alternate method 
+
+// app.get('/Home', (req, res) => {
+//     try {
+//         res.send(`
+//             <h1 style="text-align: center;">Hi, my name is Kartikay Rattan</h1> 
+//             <br> 
+//             <p>Welcome! This is my practice for backend CA.</p>
+//             <pre>${JSON.stringify(creator, null, 2)}</pre>
+//         `);  
+//     } catch (error) {
+//         res.json({ "message": "No creator found", "error": error.message });
+//     }
+// });
 
 app.listen(PORT, () => {
     console.log(`Hi, my name is Kartikay Rattan. The server is live on http://localhost:${PORT}`);
