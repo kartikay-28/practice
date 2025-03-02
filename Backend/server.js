b90216699962c4c897721eeb38c70f11294de7d0
kartikay-28 <kartikayrattan1@gmail.com>
2025-03-02 17:13:02 +0530

    Updated

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,11 @@ const PORT = 3000;
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send("This is my Express server")
+    res.send('<h1 style="text-align: center;">This is my Express server</h1>')
+})
+
+app.get('/Home',(req,res)=>{
+    res.send('<h1 style="text-align: center;">Hi, my name is Kartikay Rattan</h1> <br> <p> Welcome this is my practice for backend ca . ')
 })
 
 app.listen(PORT , () => {
