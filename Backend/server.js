ad99b060015807d01d93a004a5cf70ec1e1d59de
kartikay-28 <kartikayrattan1@gmail.com>
2025-03-03 11:25:15 +0530

    Added mongoDB connection

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const PORT = 3000;
 const app = express();
-
+const mongoose = require('mongoose');
 const creator = [
     {
         Name: "Kartikay",
@@ -35,6 +35,14 @@ app.get('/Home', (req, res) => {
 //     }
 // });
 
+mongoose.connect("mongodb+srv://kartikay002:kartikay@cluster0.wh630.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>{
+    console.log("MongoDB connected successfully")
+})
+.catch((error)=>{
+    console.error("Database connection failed",error.message);
+    process.exit(1);
+})
 app.listen(PORT, () => {
     console.log(`Hi, my name is Kartikay Rattan. The server is live on http://localhost:${PORT}`);
 });
